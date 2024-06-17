a78d4b81f17baa07ab768bedfb6dce7cceb9719a
Sanjana R <139480944+nirmadmylabcoat@users.noreply.github.com>
2024-06-17 18:11:07 +0545

    first commit

---
+++ app/src/main/java/com/example/todolistmock/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.todolistmock
+
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolistmock.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), TaskItemClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener {
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
+        }
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView()
+    {
+        val mainActivity = this
+        taskViewModel.taskItems.observe(this){
+            binding.todoListRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = TaskItemAdapter(it, mainActivity)
+            }
+        }
+    }
+
+    override fun editTaskItem(taskItem: TaskItem)
+    {
+        NewTaskSheet(taskItem).show(supportFragmentManager,"newTaskTag")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun completeTaskItem(taskItem: TaskItem)
+    {
+        taskViewModel.setCompleted(taskItem)
+    }
+}
+
+
